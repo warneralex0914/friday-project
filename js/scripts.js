1e2414bb36aac917e78d91743b97c4bd2c89ed79
Alex Warner <warneralex0914@gmail.com>
2021-01-15 13:46:40 -0800

    this commit will have a branching prompt pop up to determine which language one should learn

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,29 @@
 $(document).ready(function() {
+  $(document).ready(function() {
+    const lang = (prompt("webapps, microsoft-apps or android-apps?"));
+  
+    if (lang == "webapps") {
+      return alert("you should learn ruby")
+    }
+    if (lang == "microsoft-apps") {
+      return alert("you should learn C#")
+    }
+    if (lang == "android-apps") {
+      return alert("you should learn java")
+    }
+  });
   $("#formOne").submit(function(event) {
     event.preventDefault();
     const prompt1Input = $("input#prompt1").val();
     const prompt2Input = $("input#prompt2").val();
     const prompt3Input = $("input#prompt3").val();
     const prompt4Input = $("input#prompt4").val();
-
   
     $(".prompt1").text(prompt1Input);
     $(".prompt2").text(prompt2Input);
     $(".prompt3").text(prompt3Input);
     $(".prompt4").text(prompt4Input);
-
     $("story").show();
   });
+
 });
